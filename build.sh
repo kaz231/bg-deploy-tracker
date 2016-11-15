d039685da222e9ef6be345f95ba22468058e4966
Kamil Zajac <kamil@piwik.pro>
2016-11-15 19:29:05 +0100

    - fixed issue with invalid execution of build.sh in travis.yml

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf vendor
 
-composer install --no-dev --optimize-autoloader
+composer install --no-dev --optimize-autoloader --no-scripts
+composer run-script symfony-prod-scripts
 php bin/console cache:clear --env=prod --no-debug
-php bin/console assetic:dump --env=prod --no-debug
 rm -rf tests
 rm web/app_dev.php
